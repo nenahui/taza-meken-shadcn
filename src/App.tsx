170ea282f1a70843bbac60a5cff86548354767e4
nenahui <kanatraccoon@gmail.com>
2024-09-12 20:25:40 +0600

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
+};
